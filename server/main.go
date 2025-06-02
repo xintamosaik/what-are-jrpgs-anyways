47cb26acc1b97ef12a8b2fbaf6c487d77e76e2e1
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-02 19:47:25 +0200

    Remove old UI and web files, consolidating styles and templates into a single index.html and style.css for the Go Web RPG App.

--- server/main.go
+++ server/main.go
@@ -4,10 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-
-	"github.com/joho/godotenv"
 )
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,40 +18,13 @@ func noCache(next http.Handler) http.Handler {
 }
 
 func main() {
-	// look for a .env file in the current directory
-	// and load it into the environment variables
-	env, err := godotenv.Read(".env")
-	if err != nil {
-		fmt.Println("No .env file found, proceeding without it.")
-	}
-	if len(env) > 0 {
-		fmt.Println("Loaded environment variables from .env file.")
-		for key, value := range env {
-			fmt.Printf("%s=%s\n", key, value)
-		}
-	} else {
-		fmt.Println("No environment variables loaded from .env file.")
-	}
-	// If you have a .env file, it will load the environment variables
-	IS_DEV := env["DEV"] == "true"
-	if IS_DEV {
-		fmt.Println("Development mode is ON")
 
-		// Proxy all requests to Vite dev server
-		target, _ := url.Parse("http://localhost:5173")
-		proxy := httputil.NewSingleHostReverseProxy(target)
-
-		http.Handle("/", proxy)
-
-	} else {
-		fs := http.StripPrefix("/", http.FileServer(http.Dir("../ui/dist")))
+		fs := http.StripPrefix("/", http.FileServer(http.Dir("./web/")))
 		http.Handle("/", noCache(fs))
 
-	}
-	// Then wrap your file server:
 
 	// API route
-	http.HandleFunc("/api/ping", pingHandler)
+	http.HandleFunc("/hello", pingHandler)
 
 	fmt.Println("Starting server on :3000")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
