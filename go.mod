1a7a685370398558ad64e8f38fea465f655694d1
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-02 19:38:54 +0200

    feat: add .env support and configure environment variables for development mode

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-web-rpg-app
 
 go 1.24
+
+require github.com/joho/godotenv v1.5.1
--- server/main.go
+++ server/main.go
@@ -4,25 +4,57 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/joho/godotenv"
 )
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"message": "pong"})
 }
+func noCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	staticDir := "../ui/dist"
-
-	// Confirm static files exist
-	if _, err := os.Stat(staticDir + "/index.html"); os.IsNotExist(err) {
-		fmt.Println("ERROR: index.html not found in", staticDir)
+	// look for a .env file in the current directory
+	// and load it into the environment variables
+	env, err := godotenv.Read(".env")
+	if err != nil {
+		fmt.Println("No .env file found, proceeding without it.")
 	}
+	if len(env) > 0 {
+		fmt.Println("Loaded environment variables from .env file.")
+		for key, value := range env {
+			fmt.Printf("%s=%s\n", key, value)
+		}
+	} else {
+		fmt.Println("No environment variables loaded from .env file.")
+	}
+	// If you have a .env file, it will load the environment variables
+	IS_DEV := env["DEV"] == "true"
+	if IS_DEV {
+		fmt.Println("Development mode is ON")
 
-	fs := http.StripPrefix("/", http.FileServer(http.Dir(staticDir)))
-	http.Handle("/", fs)
+		// Proxy all requests to Vite dev server
+		target, _ := url.Parse("http://localhost:5173")
+		proxy := httputil.NewSingleHostReverseProxy(target)
 
+		http.Handle("/", proxy)
+
+	} else {
+		fs := http.StripPrefix("/", http.FileServer(http.Dir("../ui/dist")))
+		http.Handle("/", noCache(fs))
+
+	}
+	// Then wrap your file server:
+
+	// API route
 	http.HandleFunc("/api/ping", pingHandler)
 
 	fmt.Println("Starting server on :3000")
