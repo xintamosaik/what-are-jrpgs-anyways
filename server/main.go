5fdedb744d8228cac67198280c108bf2f508baaa
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-02 18:27:05 +0200

    refactor: update main handler to serve index.html and improve request logging

--- server/main.go
+++ server/main.go
@@ -11,13 +11,22 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "pong"})
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the JRPGs app!")
-}
 
 func main() {
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/api/ping", pingHandler)
+	// http.HandleFunc("/", rootHandler) // ui/dist/index.html needs to point to this
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// log the request
+		fmt.Println("Received request:", r.Method, r.URL.Path) // we do get here 
+		// But we are in /server so we need to adjust the path to serve the index.html file correctly
+		// Serve the index.html file from the ui/dist directory
+		http.ServeFile(w, r, "../ui/dist/index.html")
+	})
+	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Hello from the app!"})
+	})
+	// Register the ping handler
+	http.HandleFunc("/api/ping", pingHandler) 
 
 	fmt.Println("Starting server on :3000")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
